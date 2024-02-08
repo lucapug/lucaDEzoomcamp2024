c35376fb43816310c25364f49b5b0fcd57ed4dd2
Luca Pugliese <lucapug@outlook.it>
2024-02-08 13:12:57 +0000

    final version for yellow added explicit schema

--- week3/web_to_gcs.py
+++ week3/web_to_gcs.py
@@ -3,6 +3,7 @@ import os
 import requests
 import pandas as pd
 from google.cloud import storage
+import pyarrow as pa
 
 """
 Pre-reqs: 
@@ -16,6 +17,28 @@ init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/'
 
 # switch out the bucketname
 BUCKET = os.environ.get("GCP_GCS_BUCKET", "lucapug-taxi-data")
+
+table_schema_yellow = pa.schema(
+   [
+        ('VendorID', pa.int64()), 
+        ('tpep_pickup_datetime', pa.timestamp('s')), 
+        ('tpep_dropoff_datetime', pa.timestamp('s')), 
+        ('passenger_count', pa.int64()), 
+        ('trip_distance', pa.float64()), 
+        ('RatecodeID', pa.int64()), 
+        ('store_and_fwd_flag', pa.string()), 
+        ('PULocationID', pa.int64()), 
+        ('DOLocationID', pa.int64()), 
+        ('payment_type', pa.int64()), 
+        ('fare_amount',pa.float64()), 
+        ('extra',pa.float64()), 
+        ('mta_tax', pa.float64()), 
+        ('tip_amount', pa.float64()), 
+        ('tolls_amount', pa.float64()), 
+        ('improvement_surcharge', pa.float64()), 
+        ('total_amount', pa.float64()), 
+        ('congestion_surcharge', pa.float64())]
+)
 
 
 def upload_to_gcs(bucket, object_name, local_file):
@@ -53,9 +76,13 @@ def web_to_gcs(year, service):
         print(f"Local: {file_name}")
 
         # read it back into a parquet file
-        df = pd.read_csv(file_name, compression='gzip')
+        # df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        # df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime) 
+        parse_dates = ['tpep_pickup_datetime', 'tpep_dropoff_datetime']
+        df = pd.read_csv(file_name, compression='gzip', parse_dates=parse_dates)
         file_name = file_name.replace('.csv.gz', '.parquet')
-        df.to_parquet(file_name, engine='pyarrow')
+        
+        df.to_parquet(file_name, engine='pyarrow', schema = table_schema_yellow)
         print(f"Parquet: {file_name}")
 
         # upload it to gcs 
@@ -63,9 +90,8 @@ def web_to_gcs(year, service):
         print(f"GCS: {service}/{file_name}")
 
 
-# make the choice of the data to upload...
+# make the choice of the yearly data to upload...
 #web_to_gcs('2019', 'green')
 #web_to_gcs('2020', 'green')
-###web_to_gcs('2019', 'yellow') #lots of problems with this one!
-#web_to_gcs('2020', 'yellow')
-
+#web_to_gcs('2019', 'yellow') #lots of problems with this one, solved with schema.
+web_to_gcs('2020', 'yellow')
